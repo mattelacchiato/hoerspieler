016b976d4dec9ca6a2c69bd9718820c83568b871
Matthias Brandt <matthias.brandt@inoio.de>
2018-12-28 12:07:34 +0100

    working write (but restart failed)

--- main.py
+++ main.py
@@ -22,21 +22,119 @@ class NeedsResetException(Exception):
     pass
 
 class RFIDWrapper:
+    KEY = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
     def __init__(self):
-        self.create()
+        self._create()
 
-    def create(self):
+    def wait_for_tag(self):
+        self.rdr.wait_for_tag()
+
+    def _prepare_request(self):
+        """Returns (error, uid)"""
+        try:
+            (error, tag_type) = self.rdr.request()
+            if error: return (error, None)
+            (error, uid) = self.rdr.anticoll()
+            if error: return (error, None)
+            print("Found card UID: " + str(uid))
+            return (self.rdr.select_tag(uid), uid)
+        except:
+            self.rdr.stop_crypto()
+
+    def read_ndef_bytes(self):
+        """Returns bytes or raises exception"""
+        def get_length(block):
+            if block[4] == bytes([0]):
+                raise Exception("ndef record longer than 256 bytes. Please implement =)")
+            else:
+                return block[4]
+        
+        def read_block(block_address):
+            (error, read) = self.rdr.read(block_address)
+            if error: 
+                raise Exception("failed to read block %s" % block_address)
+            else:
+                return bytes(read)
+
+        
+        (error, uid) = self._prepare_request()
+        if error: raise Exception("failed to prepare request")
+
+        error = self.rdr.card_auth(self.rdr.auth_b, 4, self.KEY, uid)
+        if error: raise Exception("failed to auth sector 1")
+        
+        start_block = 4
+        read = read_block(start_block)
+        # ndef records starts with this sequence:
+        print("#########################")
+        print(str(read))
+
+        if read[:3] != bytes([0, 0, 3]):
+            raise Exception("Start block with invalid starting sequence: %s" % read)
+        length = get_length(read)
+        bytes_to_read = length
+        print("Found NDEF with %s length" % length)
+        ndef_bytes = read[start_block:(start_block+bytes_to_read)]
+        bytes_to_read -= 16-start_block
+        for i in range(start_block+1, 63):
+            if bytes_to_read <= 0:
+                break
+            elif i % 4 == 3:
+                #ignore every 4th blocks (reserved for key mgmt), but auth for next sector
+                error = self.rdr.card_auth(self.rdr.auth_b, i+1, self.KEY, uid)
+                if error: raise Exception("failed to auth sector %s" % i/4)
+                continue
+            else:
+                ndef_bytes += read_block(i)[:bytes_to_read]
+                bytes_to_read -= 16
+        print("found ndef bytes: %s" % str(ndef_bytes))
+        if length != len(ndef_bytes):
+            self._recreate()
+            raise Exception("Could not read all NDEF bytes (Declared: %i, got: %i)" % (length, len(ndef_bytes)))
+        return ndef_bytes
+
+    def write_ndef(self, record_bytes):
+        def zpad(list, count):
+            """padds with zeros to the end"""
+            return (list + bytes(count))[:count]
+
+        block_address = 4
+        length = len(record_bytes)
+        octets = bytes([0, 0, 3, length]) + record_bytes + b'\xFE'
+        (error, uid) = self._prepare_request()
+        if error: raise Exception("failed to prepare request")
+
+        try:
+            while len(octets) > 0:
+                if (block_address) % 4 == 0:
+                    #do auth for next sector
+                    error = self.rdr.card_auth(self.rdr.auth_b, block_address+1, self.KEY, uid)
+                    if error: raise Exception("failed to auth sector 1")
+                else:
+                    block = zpad(octets, 16)
+                    print("writing on block %i: %s" % (block_address, str(block)))
+                    self.rdr.write(block_address, block)
+                    octets = octets[16:]
+                block_address += 1
+        except:
+            self.rdr.stop_crypto()
+
+    def _create(self):
         self.rdr = RFID()
         self.util = self.rdr.util()
-        self.util.debug = False
+        self.util.debug = True
+        self.util.auth(self.rdr.auth_b, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
 
-    def recreate(self):
-        self.reset()
-        self.create()
+    #todo: check if needed (_prepare before each request should do the trick)
+    def _recreate(self):
+        self._reset()
+        self._create()
 
-    def reset(self):
+    def _reset(self):
         self.util.deauth()
         self.rdr.cleanup()
+
+
 
         
 
@@ -55,20 +153,21 @@ def cleanup():
     silently(lambda _:sp.pause_playback(device_id))
     global run
     run = False
-    wrapper.reset()
+    silently(lambda _:wrapper._reset())
     sys.exit()
 
-def read_out(block_address):
+""" def read_out(block_address):
+    #todo wtf?
     if not wrapper.util.is_tag_set_auth():
         return True
 
     error = wrapper.util.do_auth(block_address)
     if not error:
-        (error, data) = wrapper.util.rfid.read(block_address)
+        (error, data) = wrapper.rdr.read(block_address)
         return bytes(data)
     else:
         raise Exception("Error on " + wrapper.util.sector_string(block_address))
-
+ """
 # todo: read once
 
 def is_tag_present():
@@ -86,38 +185,6 @@ def is_tag_present():
     rdr.init()
     return present
 
-def read_ndef(block_address):
-    def get_length(block):
-        if block[4] == bytes([0]):
-            raise Exception("ndef record longer than 256 bytes. Please implement =)")
-        else:
-            return block[4]
-
-    read = read_out(block_address)
-    # ndef records starts with this sequence:
-    print("#########################")
-    print(str(read))
-    if read[:3] == bytes([0, 0, 3]):
-        length = get_length(read)
-        bytes_to_read = length
-        print("Found NDEF with %s length" % length)
-        # todo check if necessary
-        ndef_bytes = read[4:(4+bytes_to_read)]
-        bytes_to_read -= 16-4
-        for i in range(block_address+1, 63):
-            if bytes_to_read <= 0:
-                break
-            elif i % 4 == 3:
-                #ignore every 4th blocks (reserved for key mgmt)
-                continue
-            else:
-                ndef_bytes += read_out(i)[:bytes_to_read]
-                bytes_to_read -= 16
-        print("found ndef bytes: %s" % str(ndef_bytes))
-        if length != len(ndef_bytes):
-            wrapper.recreate()
-            raise Exception("Could not read all NDEF bytes (Declared: %i, got: %i)" % (length, len(ndef_bytes)))
-        return ndef_bytes
 
 
 def spotify_client():
@@ -141,8 +208,41 @@ def get_device_id(sp):
         else:
             continue
 
+def write2(rdr, block_address, data):
+    """
+    Writes data to block. You should be authenticated before calling write.
+    Returns error state.
+    """
+    buf = []
+    buf.append(rdr.act_write)
+    buf.append(block_address)
+    crc = rdr.calculate_crc(buf)
+    buf.append(crc[0])
+    buf.append(crc[1])
+    (error, back_data, back_length) = rdr.card_write(rdr.mode_transrec, buf)
+    if not(back_length == 4) or not((back_data[0] & 0x0F) == 0x0A):
+        print("fail1")
+        error = True
+
+    if not error:
+        buf_w = []
+        for i in range(16):
+            buf_w.append(data[i])
+
+        crc = rdr.calculate_crc(buf_w)
+        buf_w.append(crc[0])
+        buf_w.append(crc[1])
+        (error, back_data, back_length) = rdr.card_write(rdr.mode_transrec, buf_w)
+        if not(back_length == 4) or not((back_data[0] & 0x0F) == 0x0A):
+            error = True
+            print("fail2")
+
+    return error
+
+
 def write_ndef(block_address, records):
     def zpad(list, count):
+        """padds with zeros to the end"""
         return (list + bytes(count))[:count]
 
     record_bytes = b''.join((ndef.message_encoder(records)))
@@ -152,14 +252,14 @@ def write_ndef(block_address, records):
         if (block_address) % 4 != 3:
             block = zpad(octets, 16)
             print("writing on block %i: %s" % (block_address, str(block)))
-            wrapper.util.do_auth(block_address)
-            if wrapper.rdr.write(block_address, block):
+            if wrapper.util.do_auth(block_address):
+                print("Error while auth.")
+            if write2(wrapper.rdr, block_address, block):
                 print("Error while writing.")
             octets = octets[16:]
         block_address += 1
 
-def read_records():
-    octets = read_ndef(4)
+def parse_records(octets):
     records = list(ndef.message_decoder(octets))
     if records[0].type != "urn:nfc:wkt:U":
         raise Exception("Only URI records are supported. Was: "+str(records[0]))
@@ -194,48 +294,45 @@ try:
     wrapper = RFIDWrapper()
     while run:
         print("Running")
-        #cleanup_rfid()
 
         try:
-            (error, data) = wrapper.rdr.request()
-            if not error:
-                print("\nDetected: " + format(data, "02x"))
+            print("Wait for tag")
+            wrapper.wait_for_tag()
 
-            (error, uid) = wrapper.rdr.anticoll()
-            if not error:
-                wrapper.util.set_tag(uid)
-                wrapper.util.auth(wrapper.rdr.auth_b, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
-                # for i in range(0,63):
-                #    read_out(i)
-                records = read_records()
-                print(records[0].uri)
-                offset = {"uri": records[1].uri} if len(records) > 1 else None
-                sp.start_playback(device_id=device_id, context_uri=records[0].uri, offset=offset)
-                while is_tag_present():
-                    print("Tag is present")
-                    try:
-                        time.sleep(1)
-                        currently_playing = sp.currently_playing()
-                        current_track = currently_playing["item"]["uri"]
-                        print("current track: "+str(current_track))
-                        current_track_rec = ndef.UriRecord(current_track)
-                        if len(records) < 2:
-                            print("appending.")
-                            records.append(current_track_rec)
-                            write_ndef(4, records)
-                        elif current_track != records[1].uri:
-                            print("replacing " + str(records[1].uri))
-                            records[1] = current_track_rec
-                            write_ndef(4, records)
-                    except BaseException as e:
-                        # non-fatal here. simply print for debugging
-                        traceback.print_exc(file=sys.stdout)
-                print("Tag removed")
-                sp.pause_playback(device_id)
+            ndef_bytes = wrapper.read_ndef_bytes()
+            records = parse_records(ndef_bytes)
+            print(records[0].uri)
+            offset = {"uri": records[1].uri} if len(records) > 1 else None
+            sp.start_playback(device_id=device_id, context_uri=records[0].uri, offset=offset)
+            while is_tag_present():
+                print("Tag is present")
+                try:
+                    time.sleep(1)
+                    currently_playing = sp.currently_playing()
+                    current_track = currently_playing["item"]["uri"]
+                    print("current track: "+str(current_track))
+                    current_track_rec = ndef.UriRecord(current_track)
+                    if len(records) < 2:
+                        print("appending.")
+                        records.append(current_track_rec)
+                        record_bytes = b''.join((ndef.message_encoder(records)))
+                        wrapper.write_ndef(record_bytes)
+                    elif current_track != records[1].uri:
+                        print("replacing " + str(records[1].uri))
+                        records[1] = current_track_rec
+                        record_bytes = b''.join((ndef.message_encoder(records)))
+                        wrapper.write_ndef(record_bytes)
+                except BaseException as e:
+                    # non-fatal here. simply print for debugging
+                    traceback.print_exc(file=sys.stdout)
+            print("Tag removed")
+            sp.pause_playback(device_id)
             time.sleep(1)
         except BaseException as e:
             print("Ignoring Exception")
             traceback.print_exc(file=sys.stdout)
+        finally:
+            wrapper._recreate()
 except BaseException as e:
     traceback.print_exc(file=sys.stdout)
 finally:
